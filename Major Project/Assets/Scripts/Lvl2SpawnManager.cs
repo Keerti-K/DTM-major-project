7c52d2079fb99d51880ea049d605c79e4c9bc553
Keerti-K <19009@wc.school.nz>
2023-08-24 22:16:23 +1200

    Level 2 in progress
    I have started progress on level. Finished designing the 2 different obstacles for my level. They now move left along with the background. They also randomly respawn every 2 seconds.

---
+++ Major Project/Assets/Scripts/Lvl2SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl2SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclesPrefabs;
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacles()
+    {
+        int obstacleindex = Random.Range(0, obstaclesPrefabs.Length);
+        Vector3 spawnPos = new Vector3(458, 120, -40);
+        Instantiate(obstaclesPrefabs[obstacleindex], spawnPos, obstaclesPrefabs[obstacleindex].transform.rotation);
+    }
+}
